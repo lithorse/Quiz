0b9141423039c68f15ce0f5b0c1c4ca31ce88794
Otto <otto.rinaldo@gmail.com>
2018-04-29 15:29:06 +0200

    Question seeding working

--- Quiz/Models/Score.cs
+++ Quiz/Models/Score.cs
@@ -8,7 +8,7 @@ namespace Quiz.Models
     public class Score
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
+        //public int UserId { get; set; }
         public virtual ApplicationUser User { get; set; }
         public int Points { get; set; }
     }
--- Quiz/Models/SeedData.cs
+++ Quiz/Models/SeedData.cs
@@ -22,68 +22,71 @@ namespace Quiz.Models
             context.Roles.AddRange(
                 new IdentityRole
                 {
-                    Id = "0",
                     Name = "Admin"
                 },
                 new IdentityRole
                 {
-                    Id = "1",
                     Name = "Member"
                 });
 
             //TODO: Something doesn't work when seeding questions
 
-            //context.Questions.AddRange(
-            //    new Question
-            //    {
-            //        Id = 0,
-            //        Text = "Have you ever been so far as to decided to go do look more like?",
-            //        AnswerA = "Yes",
-            //        AnswerB = "No",
-            //        AnswerC = "Maybe",
-            //        AnswerD = "What?",
-            //        CorrectAnswer = "D"
-            //    },
-            //    new Question
-            //    {
-            //        Id = 1,
-            //        Text = "What is the airspeed velocity of an unladen swallow?",
-            //        AnswerA = "20 km/h",
-            //        AnswerB = "30 km/h",
-            //        AnswerC = "40 km/h",
-            //        AnswerD = "What do you mean, an african or a european swallow?",
-            //        CorrectAnswer = "D"
-            //    },
-            //    new Question
-            //    {
-            //        Id = 2,
-            //        Text = "Question 3",
-            //        AnswerA = "Answer A",
-            //        AnswerB = "Answer B",
-            //        AnswerC = "Answer E",
-            //        AnswerD = "Answer D",
-            //        CorrectAnswer = "C"
-            //    },
-            //    new Question
-            //    {
-            //        Id = 3,
-            //        Text = "If animals could talk, which would be the rudest?",
-            //        AnswerA = "Giraffe",
-            //        AnswerB = "Pigeon",
-            //        AnswerC = "Moose",
-            //        AnswerD = "Fish",
-            //        CorrectAnswer = "A"
-            //    },
-            //    new Question
-            //    {
-            //        Id = 4,
-            //        Text = "How many chickens would it take to kill an elephant",
-            //        AnswerA = "10",
-            //        AnswerB = "100",
-            //        AnswerC = "50 000",
-            //        AnswerD = "1",
-            //        CorrectAnswer = "B"
-            //    });
+            context.Questions.AddRange(
+                new Question
+                {
+                    Text = "Have you ever been so far as to decided to go do look more like?",
+                    AnswerA = "Yes",
+                    AnswerB = "No",
+                    AnswerC = "Maybe",
+                    AnswerD = "What?",
+                    CorrectAnswer = "D"
+                },
+                new Question
+                {
+                    Text = "What is the airspeed velocity of an unladen swallow?",
+                    AnswerA = "20 km/h",
+                    AnswerB = "30 km/h",
+                    AnswerC = "40 km/h",
+                    AnswerD = "What do you mean, an african or a european swallow?",
+                    CorrectAnswer = "D"
+                },
+            new Question
+            {
+                Text = "Question 3",
+                AnswerA = "Answer A",
+                AnswerB = "Answer B",
+                AnswerC = "Answer E",
+                AnswerD = "Answer D",
+                CorrectAnswer = "C"
+            },
+            new Question
+            {
+                Text = "If animals could talk, which would be the rudest?",
+                AnswerA = "Giraffe",
+                AnswerB = "Pigeon",
+                AnswerC = "Moose",
+                AnswerD = "Fish",
+                CorrectAnswer = "A"
+            },
+            new Question
+            {
+                Text = "How many chickens would it take to kill an elephant",
+                AnswerA = "10",
+                AnswerB = "100",
+                AnswerC = "50 000",
+                AnswerD = "1",
+                CorrectAnswer = "B"
+            },
+            new Question
+            {
+                Text = "Vilken låt är bäst?",
+                AnswerA = "I Wanted To Stay On The Other Side - Summer Heart",
+                AnswerB = "123 - Kaspar Kaae",
+                AnswerC = "Normalt - Det Stora Monstret",
+                AnswerD = "Paloma's Hand - Sibille Attar",
+                CorrectAnswer = "A"
+            }
+        );
 
             context.SaveChanges();
         }
